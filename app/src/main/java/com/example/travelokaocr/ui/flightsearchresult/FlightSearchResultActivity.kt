81a5fd14fbdc90dc2560ae65697319591ee6f1b4
ianindratama <ian.indratama2@gmail.com>
2022-05-15 19:41:30 +0700

    FlightSearchResultActivity and HistoryActivity added

---
+++ app/src/main/java/com/example/travelokaocr/ui/flightsearchresult/FlightSearchResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelokaocr.ui.flightsearchresult
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelokaocr.R
+
+class FlightSearchResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flight_search_result)
+    }
+}
---
+++ app/src/main/java/com/example/travelokaocr/ui/historyscreen/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelokaocr.ui.historyscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelokaocr.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}

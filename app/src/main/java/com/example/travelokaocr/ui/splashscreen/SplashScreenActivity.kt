c8cfcb1391967023b5bb50c2f6bc53666fda4fa9
Marchenda Fayza Madjid <marchendafayzam@gmail.com>
2022-05-16 19:47:16 +0800

    update OCR Screen Activity and add code scanner dependencies

--- app/src/main/java/com/example/travelokaocr/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/travelokaocr/ui/splashscreen/SplashScreenActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
@@ -29,7 +30,7 @@ class SplashScreenActivity : AppCompatActivity() {
 
     @Suppress("DEPRECATION")
     private fun setupAction() {
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashScreenActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()

eddcdd47666ffcdd945bf67788e8b16dd592e363
Marchenda Fayza Madjid <marchendafayzam@gmail.com>
2022-05-24 02:53:58 +0800

    Add variable that will be the saved for login session

---
+++ app/src/main/java/com/example/travelokaocr/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.travelokaocr.utils
+
+class Constants {
+    companion object{
+        //LOGIN
+        const val ACCESS_TOKEN = "access_token"
+        const val REFRESH_TOKEN = "refresh_token"
+        const val IS_LOGIN = "is_login"
+    }
+}

18d26fad2ba6fde4914be7414a4fcb0276d2610d
Marchenda Fayza Madjid <marchendafayzam@gmail.com>
2022-05-24 03:02:46 +0800

    Changing register response variable name

--- app/src/main/java/com/example/travelokaocr/data/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/travelokaocr/data/repository/AuthenticationRepository.kt
@@ -3,9 +3,9 @@ package com.example.travelokaocr.data.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
-import com.example.travelokaocr.data.UserData
 import com.example.travelokaocr.data.api.ApiService
 import com.example.travelokaocr.data.Result
+import com.example.travelokaocr.data.UserDataRegister
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -15,16 +15,16 @@ import java.lang.Exception
 
 class AuthenticationRepository(private val apiService: ApiService) {
 
-    fun register(name: String, email: String, password: String, profile_picture: File? = null): LiveData<Result<UserData?>?> = liveData{
+    fun register(name: String, email: String, password: String, profile_picture: File? = null): LiveData<Result<UserDataRegister?>?> = liveData{
         emit(Result.Loading)
         try {
-            val returnValue = MutableLiveData<Result<UserData?>?>()
+            val returnValue = MutableLiveData<Result<UserDataRegister?>?>()
             val response = apiService.registerUser(name, email, password, profile_picture)
             if(response.isSuccessful) {
                 returnValue.value = Result.Success(response.body())
                 emitSource(returnValue)
             } else {
-                val error = Gson().fromJson(response.errorBody()?.stringSuspending(), UserData::class.java)
+                val error = Gson().fromJson(response.errorBody()?.stringSuspending(), UserDataRegister::class.java)
                 response.errorBody()?.close()
                 returnValue.value = Result.Success(error)
                 emitSource(returnValue)
@@ -34,6 +34,9 @@ class AuthenticationRepository(private val apiService: ApiService) {
             emit(Result.Error(e.toString()))
         }
     }
+
+    suspend fun loginUser(email: String, password: String) =
+        apiService.loginUser(email, password)
 }
 
 @Suppress("BlockingMethodInNonBlockingContext")

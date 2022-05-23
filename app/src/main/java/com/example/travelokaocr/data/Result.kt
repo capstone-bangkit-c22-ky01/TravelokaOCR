9df119298c319f5b79552eff07c5cd6fcec60801
silafarsidia <silafarsidia57@gmial.com>
2022-05-24 01:07:31 +0800

    Adding Result.kt

---
+++ app/src/main/java/com/example/travelokaocr/data/Result.kt
@@ -0,0 +1,9 @@
+package com.example.travelokaocr.data
+
+sealed class Result<out R> private constructor() {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val error: String) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+
+}

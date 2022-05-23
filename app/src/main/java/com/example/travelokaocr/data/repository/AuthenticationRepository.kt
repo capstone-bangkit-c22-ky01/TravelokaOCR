84202520688732768e089183f1bc15220610248f
silafarsidia <silafarsidia57@gmial.com>
2022-05-24 01:27:23 +0800

    Adding Lifecycle Dependencies

--- app/src/main/java/com/example/travelokaocr/data/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/travelokaocr/data/repository/AuthenticationRepository.kt
@@ -1,13 +1,14 @@
 package com.example.travelokaocr.data.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import com.example.travelokaocr.data.UserData
 import com.example.travelokaocr.data.api.ApiService
 import java.io.File
 
 class AuthenticationRepository(private val apiService: ApiService) {
 
-    fun register(name: String, email: String, password: String, profile_picture: File? = null){
+    fun register(name: String, email: String, password: String, profile_picture: File? = null): LiveData<Result<UserData?>> = liveData{
 
     }
 }

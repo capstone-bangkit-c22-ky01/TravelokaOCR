752dd710c5a979b4fadf60325df03670ce868b01
silafarsidia <silafarsidia57@gmial.com>
2022-05-23 22:28:10 +0800

    Adding RegisterResponse.kt

---
+++ app/src/main/java/com/example/travelokaocr/data/RegisterResponse.kt
@@ -0,0 +1,33 @@
+package com.example.travelokaocr.data
+
+import com.google.gson.annotations.SerializedName
+import java.io.File
+
+data class RegisterResponse(
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("email")
+    val email: String,
+
+    @field:SerializedName("password")
+    val password: String,
+
+    @field:SerializedName("foto_profil")
+    val profile_picture: File? = null
+
+)
+
+data class UserData(
+
+    @field:SerializedName("status")
+    val status: String,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("user_id")
+    val user_id: String
+
+)

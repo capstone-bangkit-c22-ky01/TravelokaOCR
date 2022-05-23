c97c9e5aa8a416f43d941ad34f92c0c377920f5c
Marchenda Fayza Madjid <marchendafayzam@gmail.com>
2022-05-24 02:56:09 +0800

    Update authentication view model for login

--- app/src/main/java/com/example/travelokaocr/viewmodel/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/travelokaocr/viewmodel/AuthenticationViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.travelokaocr.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.travelokaocr.data.models.LoginResponse
+import com.example.travelokaocr.data.repository.AuthenticationRepository
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
-class AuthenticationViewModel: ViewModel() {
+class AuthenticationViewModel(private val authenticationRepository: AuthenticationRepository): ViewModel() {
+    //LOGIN
+    val loginUsers: MutableLiveData<Response<LoginResponse>> = MutableLiveData()
+
+    fun getLoginUsersResponse(email: String, password: String) =
+        viewModelScope.launch {
+            val response = authenticationRepository.loginUser(email, password)
+            loginUsers.value = response
+        }
 }

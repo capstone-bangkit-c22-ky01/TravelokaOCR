95f2edcd51afe48d0ad0d05d52108943811d47c3
silafarsidia <silafarsidia57@gmial.com>
2022-05-24 02:00:19 +0800

    Sync dependencies and finishing register fun in AuthenticationRepository.kt

--- app/src/main/java/com/example/travelokaocr/data/repository/AuthenticationRepository.kt
+++ app/src/main/java/com/example/travelokaocr/data/repository/AuthenticationRepository.kt
@@ -1,14 +1,40 @@
 package com.example.travelokaocr.data.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
 import com.example.travelokaocr.data.UserData
 import com.example.travelokaocr.data.api.ApiService
+import com.example.travelokaocr.data.Result
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.ResponseBody
 import java.io.File
+import java.lang.Exception
 
 class AuthenticationRepository(private val apiService: ApiService) {
 
-    fun register(name: String, email: String, password: String, profile_picture: File? = null): LiveData<Result<UserData?>> = liveData{
-
+    fun register(name: String, email: String, password: String, profile_picture: File? = null): LiveData<Result<UserData?>?> = liveData{
+        emit(Result.Loading)
+        try {
+            val returnValue = MutableLiveData<Result<UserData?>?>()
+            val response = apiService.registerUser(name, email, password, profile_picture)
+            if(response.isSuccessful) {
+                returnValue.value = Result.Success(response.body())
+                emitSource(returnValue)
+            } else {
+                val error = Gson().fromJson(response.errorBody()?.stringSuspending(), UserData::class.java)
+                response.errorBody()?.close()
+                returnValue.value = Result.Success(error)
+                emitSource(returnValue)
+            }
+        }
+        catch (e: Exception) {
+            emit(Result.Error(e.toString()))
+        }
     }
 }
+
+@Suppress("BlockingMethodInNonBlockingContext")
+suspend fun ResponseBody.stringSuspending() = withContext(Dispatchers.IO) { string() }

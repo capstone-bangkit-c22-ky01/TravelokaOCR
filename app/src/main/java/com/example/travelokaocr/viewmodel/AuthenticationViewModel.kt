6858b78e7f8d22e57bc7113c07a5f7e0b6a3a7c6
silafarsidia <silafarsidia57@gmial.com>
2022-05-24 02:03:50 +0800

    Adding fun registerUser in AuthenticationViewModel.kt

--- app/src/main/java/com/example/travelokaocr/viewmodel/AuthenticationViewModel.kt
+++ app/src/main/java/com/example/travelokaocr/viewmodel/AuthenticationViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.travelokaocr.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.travelokaocr.data.repository.AuthenticationRepository
+import java.io.File
 
-class AuthenticationViewModel: ViewModel() {
+class AuthenticationViewModel(private val authenticationRepository: AuthenticationRepository): ViewModel() {
+
+    fun registerUser(name: String, email: String, password: String, profile_picture: File? = null) = authenticationRepository.register(name, email, password, profile_picture)
 }

1374bc49e60ba00e9e26b749c5594c45bf9621a5
silafarsidia <silafarsidia57@gmial.com>
2022-05-25 15:53:13 +0800

    Adding Google Btn Functionality in RegisterActivity.kt

--- app/src/main/java/com/example/travelokaocr/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/travelokaocr/ui/register/RegisterActivity.kt
@@ -1,8 +1,10 @@
 package com.example.travelokaocr.ui.register
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.view.View
 import android.view.WindowInsets
@@ -18,6 +20,14 @@ import com.example.travelokaocr.ui.flightscreen.FlightActivity
 import com.example.travelokaocr.ui.login.LoginActivity
 import com.example.travelokaocr.viewmodel.AuthenticationViewModel
 import com.example.travelokaocr.viewmodel.factory.AuthenticationViewModelFactory
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+
+
+const val RC_SIGN_IN = 123
 
 class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityRegisterBinding
@@ -116,10 +126,33 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_sign_up -> {
+                registerForm()
                 startActivity(Intent(this, FlightActivity::class.java))
             }
             R.id.btn_login_with_google -> {
-                registerForm()
+                // Configure sign-in to request the user's ID, email address, and basic
+                // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+                // Configure sign-in to request the user's ID, email address, and basic
+                // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build()
+
+                // Build a GoogleSignInClient with the options specified by gso.
+                val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+                val acct = GoogleSignIn.getLastSignedInAccount(this)
+                if (acct != null) {
+                    val personName = acct.displayName
+                    val personGivenName = acct.givenName
+                    val personFamilyName = acct.familyName
+                    val personEmail = acct.email
+                    val personId = acct.id
+                    val personPhoto: Uri? = acct.photoUrl
+                }
+
+                val signInIntent: Intent = mGoogleSignInClient.getSignInIntent()
+                startActivityForResult(signInIntent, RC_SIGN_IN)
             }
             R.id.login -> {
                 startActivity(Intent(this, LoginActivity::class.java))
@@ -171,6 +204,31 @@ class RegisterActivity : AppCompatActivity(), View.OnClickListener {
                     Toast.LENGTH_LONG)
                     .show()
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Google Auth", "signInResult:failed code=" + e.statusCode)
         }
     }
 }

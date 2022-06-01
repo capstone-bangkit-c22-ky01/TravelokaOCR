63ca4ab0f96bba8f79d7978a53d12b1b5738fc8f
silafarsidia <silafarsidia57@gmail.com>
2022-05-31 19:29:11 -0700

    Adding Access Profile Factory

---
+++ app/src/main/java/com/example/travelokaocr/viewmodel/factory/AccessProfileFactory.kt
@@ -0,0 +1,14 @@
+package com.example.travelokaocr.viewmodel.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.travelokaocr.data.repository.AccessProfileRepository
+import com.example.travelokaocr.viewmodel.AccessProfileViewModel
+import com.example.travelokaocr.viewmodel.AuthViewModel
+
+class AccessProfileFactory(private val repo: AccessProfileRepository)
+    : ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return AccessProfileViewModel(repo) as T
+        }
+    }

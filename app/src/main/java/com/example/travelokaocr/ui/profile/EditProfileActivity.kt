06fa2538167f67c366a805ae837d67655d782c5f
silafarsidia <silafarsidia57@gmail.com>
2022-06-02 23:28:43 -0700

    Updating upload photo profile

--- app/src/main/java/com/example/travelokaocr/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/com/example/travelokaocr/ui/profile/EditProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.example.travelokaocr.ui.profile
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,14 +10,21 @@ import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.travelokaocr.R
 import com.example.travelokaocr.data.repository.AccessProfileRepository
 import com.example.travelokaocr.databinding.ActivityEditProfileBinding
 import com.example.travelokaocr.utils.Resources
+import com.example.travelokaocr.utils.reduceFileImage
+import com.example.travelokaocr.utils.uriToFile
 import com.example.travelokaocr.viewmodel.AccessProfileViewModel
 import com.example.travelokaocr.viewmodel.factory.AccessProfileFactory
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -26,6 +34,18 @@ class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
     //API
     private lateinit var viewModel: AccessProfileViewModel
 
+    private var getFile: File? = null
+
+    private val username = binding.edtUsername.text.toString()
+    private val email = binding.edtEmail.text.toString()
+    private val fotoProfil = binding.ivProfilePicture.toString()
+
+    private val dataUser = hashMapOf(
+        "name" to username,
+        "email" to email,
+        "foto_profil" to fotoProfil
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -34,15 +54,7 @@ class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
         setupView()
         itemOnClickListener()
 
-        val username = binding.edtUsername.text.toString()
-        val email = binding.edtEmail.text.toString()
-        val fotoProfil = binding.ivProfilePicture.toString()
 
-        val dataUser = hashMapOf(
-                "name" to username,
-                "email" to email,
-                "foto_profil" to fotoProfil
-        )
         showDataUser(dataUser)
 
         //CREATE API CONNECTION
@@ -174,6 +186,63 @@ class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+
+            val myFile = uriToFile(selectedImg, this@EditProfileActivity)
+
+            getFile = myFile
+
+            binding.ivProfilePicture.setImageURI(selectedImg)
+        }
+    }
+
+    private fun uploadImage(dataUser: HashMap<String, String>) {
+        if (getFile != null) {
+            val file = reduceFileImage(getFile as File)
+
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+
+            viewModel.updateUser(dataUser).observe(this){response ->
+                if (response is Resources.Loading) {
+                    progressBar(true)
+                } else if (response is Resources.Error) {
+                    progressBar(false)
+                    Toast.makeText(this, response.error, Toast.LENGTH_SHORT).show()
+                } else if (response is Resources.Success) {
+                    progressBar(false)
+                    val result = response.data
+                    if (result != null) {
+                        if (result.status.equals("success")) {
+                            val image = result.data?.foto_profil.toString()
+                            Toast.makeText(this@EditProfileActivity, "success", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Log.d("PROFILE", result.message.toString())
+                        }
+                    } else {
+                        Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
     private fun update() {
         val username = binding.edtUsername.text.toString()
         val email = binding.edtEmail.text.toString()
@@ -217,10 +286,11 @@ class EditProfileActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(Intent(this, ProfileActivity::class.java))
             }
             R.id.tv_upload_image -> {
-
+                startGallery()
             }
             R.id.btn_save_changes -> {
                 update()
+                uploadImage(dataUser)
                 startActivity(Intent(this, ProfileActivity::class.java))
             }
         }

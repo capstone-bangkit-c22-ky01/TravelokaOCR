5682652fdce23665417c2d360a278264967f9cb9
silafarsidia <silafarsidia57@gmail.com>
2022-06-07 14:46:17 -0700

    Creating Eula Page

---
+++ app/src/main/java/com/example/travelokaocr/ui/eula/EulaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelokaocr.ui.eula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travelokaocr.R
+
+class EulaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eula)
+    }
+}

8f5c96fb5ccf23c494f959dba197a295d9649c26
ianindratama <ian.indratama2@gmail.com>
2022-06-15 20:22:36 +0700

    fix bugs in OCRScreenActivity and refactor code in OCRResultActivity

--- app/src/main/java/com/example/travelokaocr/ui/ocr/SuccessPageActivity.kt
+++ app/src/main/java/com/example/travelokaocr/ui/ocr/SuccessPageActivity.kt
@@ -37,7 +37,6 @@ class SuccessPageActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
         val intent = Intent(this, HomeActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
